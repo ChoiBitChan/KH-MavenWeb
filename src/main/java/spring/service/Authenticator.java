490e406d360ae70bd3909933929ab9828ee42dd5
Chan <Chan@211.222.236.142>
2017-03-27 01:00:30 +0900

    .

--- src/main/java/spring/service/Authenticator.java
+++ src/main/java/spring/service/Authenticator.java
@@ -1,7 +1,5 @@
 package spring.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 public interface Authenticator {
 	
 	void authenticate(LoginCommand loginCommand) throws AuthenticationException;
